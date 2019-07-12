d7b3770a20eb8a3ba136572e5f1884bebc389998
Daniel Sullivan <dansullivan19@gmail.com>
2019-07-12 02:30:55 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -51,15 +51,15 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-​
+
 end
-​
+
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
   movies = Movie.where("release_date > ?", 2002).order(release_date: :desc)
 end
-​
+
 def can_be_found_updated_and_saved
   # Update the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   Movie.create(title: "Awesome Flick")
@@ -68,14 +68,14 @@ def can_be_found_updated_and_saved
   movie.save
   movie
 end
-​
+
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
   movie = Movie.find_by(title: "Wat?")
   movie.update(title: "Wat, huh?")
 end
-​
+
 def can_update_multiple_items_at_once
   # Change title of all movies to "A Movie"
   5.times do |i|
@@ -83,12 +83,12 @@ def can_update_multiple_items_at_once
   end
   Movie.update_all "title = 'A Movie'"
 end
-​
+
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once").destroy
 end
-​
+
 def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
